669bb1b10157285ccbe053b3630df64cb94656fe
Rashed <rashedulhoqueceo@gmail.com>
2023-12-21 12:51:19 +0600

    create register page

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import Scroll from "../Components/Scroll";
 AOS.init();
 
 const Main = () => {
     return (
         <div>
+            <Scroll/>
             <Navbar/>
             <Outlet/>
         </div>
